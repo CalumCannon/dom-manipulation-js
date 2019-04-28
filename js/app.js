f6a6bece5a620c41e9f0388f592263121e9f4642
CalumCannon <calbo90@googlemail.com>
2019-04-28 20:56:29 +0100

    adds extension

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
+  
+    const form = document.querySelector('#formInput');
 
 
-  const form = document.querySelector('#formInput');
+    const handleFormSubmit = function (event) {
 
-
-  const handleFormSubmit = function (event) {
-    console.log("HELLO");
     //Prevent page refresh
     event.preventDefault();
 
@@ -14,8 +12,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const artistValue =  event.target.artist.value;
     // Getting Author Input Value
     const albumValue =  event.target.albumTitle.value;
-    //Getting dropdown value
+    //Getting release date value
     const releaseDate = event.target.releaseDate.value;
+    //Getting dropdown value
+    const category = event.target.category.value;
 
     //CREATING DIV CONTAINER
     const divContainer = document.createElement('div');
@@ -36,30 +36,15 @@ document.addEventListener('DOMContentLoaded', () => {
       generateElement('h1', artistValue,divContainer);
       generateElement('p',albumValue,divContainer);
       generateElement('p',releaseDate,divContainer);
+      generateElement('p',category,divContainer);
       divContainer.classList.add('divbox');
     }
 
     generateBlock();
-
-    /*
-    //CREATE ELEMENT / SET VALUE / ATTACH TO DIV
-    const pTitle = document.createElement('p');
-    pTitle.textContent = artistValue;
-    divContainer.appendChild(pTitle);
-
-    //CREATE ELEMENT / SET VALUE / ATTACH TO DIV
-    const pAuthor = document.createElement('p');
-    pAuthor.textContent = albumValue;
-    divContainer.appendChild(pAuthor);
-
-    //CREATE ELEMENT / SET VALUE / ATTACH TO DIV
-    const pCategory = document.createElement('p');
-    pCategory.textContent = releaseDate;
-    divContainer.appendChild(pCategory);
-    */
   }
   form.addEventListener('submit', handleFormSubmit);
 
+  //DELETE BUTTON
   const deleteBtn = document.querySelector('#deleteAll');
   deleteAllListItems = () => {
     readingList.textContent = "";
